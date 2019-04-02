bbe52e2094538e708b4eafbb63e934b3fe3efe3d
Isaac Smart <ismart53755@gmail.com>
2019-04-02 23:37:10 +0000

    A code to encrypt however whitespace is an issue

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+
+
 int main() {
-    int array[500];
+    char str[500] = {"abcde"};//Intiialising Word
+    int i;
+    int key = 1; //Roational Factor
+        for(i=0; i < 500 && str[i] != 0; i++) {
+            //For loop to get every value of the array
+            if(str[i] >= 97 && str[i] <= 122) { 
+                str[i] = str[i] - 32; //IF statement make Capital
+            }
+            str[i] = str[i]+ key; //Rotational Factor
+            if(str[i] > 90) {
+                str[i] = str[i] - 26;
+            }
+        }
+    printf("%s\n", str);
+    
     
     return 0;
 }
