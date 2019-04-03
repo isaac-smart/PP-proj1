c6569a5f9ee4ad7d90e1cd687d229791b38249cc
Isaac Smart <ismart53755@gmail.com>
2019-04-03 00:50:17 +0000

    With switch case

--- main.c
+++ main.c
@@ -1,10 +1,26 @@
 #include <stdio.h>
 
+int Encryption(void);
 int main() {
-    char str[500] = {"I made it work"};//Intiialising Word
+    char c;
+    printf("Select an option: \na) Rotation Cypher\n");
+    printf("b) Rotation Cypher Attack\nc) Substitution Cypher\n");
+    printf("d) Substitution Cypher Attack\nSelection: ");
+    scanf("%c", &c);
+    switch(c) {
+        case'a': Encryption();
+            break;
+        default: 
+            printf("Unknown Selection\n");
+    }  
+    return 0;
+}
+
+int Encryption(void) {
+    char str[500] = {"ABCDE fegh"};//Intiialising Word
     int i;
     int key = 1; //Rotational Factor
-        //For loop to get every value of the array
+        //For loop to get every value of the string
         for(i=0; i < 500 && str[i] != 0; i++) {
             if(str[i] != 32) { //If whitespace no change occurs
                 if(str[i] >= 97 && str[i] <= 122) { 
@@ -19,7 +35,5 @@ int main() {
             }
         }
     printf("%s\n", str);
-    
-    
     return 0;
 }
