f810dc6f1cd333b3684d78a5fc802720cb5d1ddd
Isaac Smart <ismart53755@gmail.com>
2019-04-04 23:42:42 +0000

    Decrypt and encrypt via rotation. Punctuation and numerals aren't considered.

--- main.c
+++ main.c
@@ -2,15 +2,20 @@
 
 int Encryption(void);
 int Decryption(void);
+int Sub_Cypher(void);
 
 int main() {
     char c;
     printf("Select an option: \na) Rotation Cypher\n");
-    printf("b) Rotation Cypher Attack\nc) Substitution Cypher\n");
-    printf("d) Substitution Cypher Attack\nSelection: ");
+    printf("b) Rotation Cypher Attack(known key\nc) Substitution Cypher\n");
+    printf("d) Substitution Cypher Attack(known key)\nSelection: ");
     scanf("%c", &c);
     switch(c) {
-        case'a': Encryption();
+        case 'a': Encryption();
+            break;
+        case 'b': Decryption();
+            break;
+        case 'c': Sub_Cypher();
             break;
         default: 
             printf("Unknown Selection\n");
@@ -20,17 +25,19 @@ int main() {
 
 int Encryption(void) {
     char str[500];//Intiialising Word
+    int key;
+    printf("\nThe key of decryption:\n");
+    scanf("%d", &key);
     printf("\nPhrase to be encrypted:\n");
-    scanf("%[^n]", str );
+    scanf("%[^\n]s", str);
     int i;
-    int key = 1; //Rotational Factor
         //For loop to get every value of the string
         for(i=0; i < 500 && str[i] != 0; i++) {
             if(str[i] != 32) { //If whitespace no change occurs
                 if(str[i] >= 97 && str[i] <= 122) { 
                     str[i] = str[i] - 32; //IF statement make Capital
                 }
-                str[i] = str[i]+ key; //Encrypting
+                str[i] = str[i] + key; //Encrypting
                 if(str[i] > 90) {
                     /*If ASCII number exceeds 90 and is not going to 
                     produce letter minus 26 to get letter*/
@@ -42,4 +49,28 @@ int Encryption(void) {
     return 0;
 }
 
+int Decryption() {
+    char str1[500];//Intiialising Decrytpion
+    int key;
+    printf("\nThe key of decryption:\n");
+    scanf("%d", &key);
+    printf("\nPhrase to be decrypted:\n");
+    scanf("%[^\n]s", str1);
+    int i;
+        //For loop to get every value of the string
+    for(i=0; (i < 26) && (str1[i] != 0); i++) {
+        if(str1[i] != 32) {
+            str1[i] = str1[i] - key;
+            if(str1[i] < 65) {
+                str1[i] = str1[i] + 26;
+            }
+        }
 
+    }
+    printf("%s\n", str1);
+    return 0;
+}
+
+int Sub_Cypher(void) {
+    
+}
