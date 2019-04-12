01dd26b5dd34388992bccc2fc3e69b987a3aa551
Isaac Smart <ismart53755@gmail.com>
2019-04-12 01:25:56 +0000

    Substitution cypher complete except for ease of not including

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 int Encryption(void);
 int Decryption(void);
 int Sub_Cypher(void);
+int Sub_Decryption(void);
 
 int main() {
     char c;
@@ -17,7 +18,9 @@ int main() {
             break;
         case 'c': Sub_Cypher();
             break;
-        default: 
+        case 'd': Sub_Decryption();
+            break;
+        default:
             printf("Unknown Selection\n");
     }  
     return 0;
@@ -32,7 +35,7 @@ int Encryption(void) {
     int i;
         //For loop to get every value of the string
         for(i=0; i < 500 && str[i] != 0; i++) {
-            if(str[i] < 65);
+            if(str[i] < 65); str[25]
             else { //If whitespace no change occurs
                 if(str[i] >= 97 && str[i] <= 122) { 
                     str[i] = str[i] - 32; //IF statement make Capital
@@ -75,15 +78,20 @@ int Sub_Cypher(void) {
     char str[500];//Intiialising Word
     char key[26];
     int keysize;
-    printf("\nKey of encryption:(only lowercase letters)\n <enter> Size of key inputted:\n");
+    printf("\nKey of encryption:\n <enter> Size of key inputted:\n");
+    printf("<Space> Phrase to be encrypted:\n");
     scanf("%s %d", key, &keysize);
-    printf("Phrase to be encrypted:");
     scanf("%[^\n]s", str);
     printf("\n");
     int i;//key into code
-    int a = 97, z = 122, t; //a is the start usuallly a, z is the end
+    int a = 65, z = 90; //the start usuallly a, z is the end
     char fullkey[26];
     int p;
+    for(i = 0; i < 26; i++) {
+        if(key[i] >= 97) {
+            key[i] = key[i] - 32;
+        }
+    }
     for( i = 0; i < 26; i++) { //for statement to intialise fullkey
         if(keysize > i) { //if part of word at code beginning
             fullkey[i] = key[i]; // makes it eqaul new key
@@ -95,20 +103,41 @@ int Sub_Cypher(void) {
             }
         }
         else {
-            t = 0;
-            fullkey[i] = a; //Full key eqauls a
-            for(p = 0; p <= keysize; p++) { //to intialise key outside the input
-                if(fullkey[p] == fullkey[i] ) { //however if equals something already in key
+            fullkey[i] = a; //Full key equal a for if statement
+            for(p = 0; (p < keysize); p++) { //to intialise key outside the input
+                    if(fullkey[p] == fullkey[i]) {
                     a++;
+                    fullkey[i] = a;
+                    p = -1;
+//If any value in key equals the next desired number the number next in 
+//the alphabet is subbed into the key and the loop begins again
                 }
-                
             }
-            fullkey[i] = a;
-            a++;
+            fullkey[i] = a; //make fullkey equal a again
+            a++;//add for next number
+        }
+    }   
+    
+    for(i=0; i < 500; i++) {
+        if( islower(str[i])) {
+            str[i] = str[i] - 32;
+        }
+    }
+    for(i=0; i < 500; i++) {
+        if( isupper(str[i])) { //only code not whitespace or grammar
+            str[i] = str[i] - 65; // minus 65 to get number 0-25
+            int p;
+            p = str[i]; //make p equal this number
+            str[i] = fullkey[p]; 
 
         }
-
     }
-    printf("%s", fullkey);
+    printf("Encryption:\n");
+    
+    printf("%s\n", str);
+    
     return 0;
 }
+ int Sub_Decryption(void) {
+     
+ }
